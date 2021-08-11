7e589599b74618b90f7c674c5a83229381faa8c8
Yash <yash.tiwari3565@gmail.com>
2021-08-11 17:28:23 +0530

    added to research area , drop down in gallery

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,13 +1,14 @@
-// const showAnim = gsap.from('.main-tool-bar', { 
-//     yPercent: -100,
-//     paused: true,
-//     duration: 0.3
-//   }).progress(1);
-  
-//   ScrollTrigger.create({
-//     start: "top top",
-//     end: 99999,
-//     onUpdate: (self) => {
-//       self.direction === -1 ? showAnim.play() : showAnim.reverse()
-//     }
-//   });
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 17.5947, lng: 78.1230 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,35 +1,51 @@
-let items = document.querySelectorAll('.sidebarli');
+// let items = document.querySelectorAll(".sidebarli");
 
-items.forEach(item => {
-  item.addEventListener('click', () => {
-     items.forEach(item => item.classList.remove('active'))
-      item.classList.add('active')
-  })
-})
+// items.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     items.forEach((item) => item.classList.remove("active"));
+//     item.classList.add("active");
+//   });
+// });
 
 window.onload = () => {
-  sessionStorage.setItem('prev', 1);
-}
-function change(id)
-{
-  let prev = sessionStorage.getItem('prev', 1);// integer like 1, 2
-  if(id!=prev)
-  {
-      document.getElementById(id).classList.remove('d-none');
-      document.getElementById(`gal-${id}`).classList.add('active');
-      document.getElementById(prev).classList.add('d-none');
-      document.getElementById(`gal-${prev}`).classList.remove('active');
-      sessionStorage.setItem('prev', id);
+  sessionStorage.setItem("prev", 1);
+};
+function change(id) {
+  let prev = sessionStorage.getItem("prev", 1); // integer like 1, 2
+  if (id != prev) {
+    document.getElementById(id).classList.remove("d-none");
+    document.getElementById(`gal-${id}`).classList.add("active");
+    document.getElementById(prev).classList.add("d-none");
+    document.getElementById(`gal-${prev}`).classList.remove("active");
+    sessionStorage.setItem("prev", id);
   }
 }
 
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelectorAll('.sidebar .nav-link').forEach(function(element){
+    
+    element.addEventListener('click', function (e) {
 
+      let nextEl = element.nextElementSibling;
+      let parentEl  = element.parentElement;	
 
-
-
-
-
-
-
-
-
+        if(nextEl) {
+            e.preventDefault();	
+            let mycollapse = new bootstrap.Collapse(nextEl);
+            
+            if(nextEl.classList.contains('show')){
+              mycollapse.hide();
+            } else {
+                mycollapse.show();
+                // find other submenus with class=show
+                var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                // if it exists, then close all of them
+                if(opened_submenu){
+                  new bootstrap.Collapse(opened_submenu);
+                }
+            }
+        }
+    }); // addEventListener
+  }) // forEach
+}); 
+// DOMContentLoaded  end
